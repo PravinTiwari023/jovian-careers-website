2720cc6fa681cd247e54532a125b33e74b1bfd8a
Pravin Tiwari <tiwaripravin114@gmail.com>
2024-04-23 18:54:20 +0530

    0010 commit

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from database import load_jobs_from_db
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    # Handle the root route ("/") and render the "home.html" template with the loaded jobs.
+    # Load the jobs from the database using the load_jobs_from_db() function
     JOBS = load_jobs_from_db()
+
+    # Render the "home.html" template and pass the loaded jobs as a variable named "jobs"
     return render_template("home.html", jobs=JOBS)
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0" , debug=True)
+@app.route("/api/jobs")
+def list_jobs():
+    # Handle the "/api/jobs" route and return the loaded jobs as a JSON response.
+    # Load the jobs from the database using the load_jobs_from_db() function
+    JOBS = load_jobs_from_db()
+
+    # Return the loaded jobs as a JSON response
+    return jsonify(JOBS)
+
+
+if __name__ == "__main__":
+    # Run the Flask application in debug mode, listening on all available network interfaces.
+    app.run(host="0.0.0.0", debug=True)
--- database.py
+++ database.py
@@ -6,15 +6,22 @@ pgsql_data = "postgresql://joviancareers_zmpb_user:fdliuVsnjQ7kn82aa2Juuq9vU6d6k
 # Create the SQLAlchemy engine
 engine = create_engine(pgsql_data)
 
+
 def load_jobs_from_db():
+    """
+    Load jobs from the PostgreSQL database and return them as a list of dictionaries.
+    """
     # Attempt to connect to the database
     with engine.connect() as conn:
         # If the connection is successful, print a success message
         print("Connection to PostgreSQL database successful!")
+
+        # Execute the SQL query to select all rows from the "jobs" table
         result = conn.execute(text("SELECT * FROM jobs"))
 
         result_dict = []
 
+        # Iterate over each row in the result and convert it to a dictionary
         for row in result.all():
             result_dict.append({
                 'id': row[0],
@@ -27,4 +34,6 @@ def load_jobs_from_db():
                 'created_at': row[7],
                 'updated_at': row[8]
             })
+
+    # Return the list of dictionaries representing the jobs
     return result_dict
